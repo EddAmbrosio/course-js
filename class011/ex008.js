92e0c0ce973fa35b58c96a48d801e4f4d3f3cee9
Edmilson Santos de Almeida <99562810+EddAmbrosio@users.noreply.github.com>
2022-03-19 18:57:34 -0300

    introduce to conditions

---
+++ class011/ex008.js
@@ -0,0 +1,6 @@
+var speed = 50.5
+console.log(`The speed of your car is ${speed}!`)
+if (speed > 60) {
+    console.log(`Your speed was ${speed}, you exceeded the highway speed limit!!! And you'll take a fine for driving too fast!`)
+}
+console.log(`You must always drive with security belt!!`)
---
+++ class011/ex009.js
@@ -0,0 +1,7 @@
+country = "Japan"
+console.log(`Living in the ${country}!`)
+if (country == "Brazil") {
+    console.log("Brazilian!")
+} else {
+    console.log("Foreigner!")
+}
