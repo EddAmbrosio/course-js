0f40def6d94a6871e37e8d27b173248c48221cf6
Edmilson Santos de Almeida <99562810+EddAmbrosio@users.noreply.github.com>
2022-03-26 22:45:19 -0300

    Update function05.js

--- class016/function05.js
+++ class016/function05.js
@@ -1,20 +1,10 @@
 //RECURSIVIDADE
-/*function factorial(n) {
+function factorial(n) {
     if (n == 1) {
         return 1
     } else {
-        return n * factorial(n - 1)
+        return n * factorial(n - 1) // here we have a loop, until n == 1!
     }
 }
 
-console.log(factorial(5))*/
-
-function factorial(n) {
-    let fact = 1
-    for(n = 5; n >1; n--) {
-        fact *= n
-    }
-    return fact
-}
-
-console.log(factorial(8))
+console.log(factorial(5))
